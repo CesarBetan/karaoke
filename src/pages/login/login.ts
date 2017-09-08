03b5c34fcb08aec0fcd8c2355fa16e9f34fefa48
Cesar Betancourt <cesarebetan@gmail.com>
2017-09-07 23:24:36 -0500

    Home created

---
+++ src/pages/login/login.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { HomePage } from '../home/home';
+
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html'
+})
+export class LoginPage {
+  homePage = HomePage;
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
